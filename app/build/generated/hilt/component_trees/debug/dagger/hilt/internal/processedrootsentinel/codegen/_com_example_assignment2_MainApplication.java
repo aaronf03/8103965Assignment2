aaa60f3f0b5e8b39dc578316a385b65209fbe1ad
AaronFinlay <aaronjfinlay@hotmail.com>
2026-09-14 21:57:16 +1000

    Reapply "FIx .GitIgnore"
    This reverts commit ad69722225ef731bed3f46598569a9885c065e0a.

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_example_assignment2_MainApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.example.assignment2.MainApplication"
-)
-public final class _com_example_assignment2_MainApplication {
-}
